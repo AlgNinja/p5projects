c930106b284b8b7d5c923f65263af584337726a4
Kesler <45579249+OnlineEXP@users.noreply.github.com>
2019-10-01 19:51:58 -0700

    Update game.js

--- javascript/snake/game.js
+++ javascript/snake/game.js
@@ -5,28 +5,6 @@ function snake(length, history) {
   this.directionX = blocksize * 1;
   this.directionY = 0;
   //creates a basis for the snake including x,y, snake history, and length
-  this.move() = function() {
-    if (keyIsDown(87) && this.directionY != blocksize) {
-      this.directionX = 0;
-      this.directionY = blocksize * -1;
-      //w
-    }
-    if (keyIsDown(65) && this.directionX != blocksize) {
-      this.directionX = blocksize * -1;
-      this.directionY = 0;
-      //a
-    }
-    if (keyIsDown(68) && this.directionX != blocksize * -1) {
-      this.directionX = blocksize;
-      this.directionY = 0;
-      //d
-    }
-    if (keyIsDown(83) && this.directionY != blocksize * -1) {
-      this.directionX = 0;
-      this.directionY = blocksize;
-      //s
-    }
-
   }
 }
 
@@ -38,6 +16,26 @@ function setup() {
 }
 
 function draw() {
+  if (keyIsDown(87) && s.directionY != blocksize) {
+    s.directionX = 0;
+    s.directionY = blocksize * -1;
+    //w
+  }
+  if (keyIsDown(65) && s.directionX != blocksize) {
+    s.directionX = blocksize * -1;
+    s.directionY = 0;
+    //a
+  }
+  if (keyIsDown(68) && s.directionX != blocksize * -1) {
+    s.directionX = blocksize;
+    s.directionY = 0;
+    //d
+  }
+  if (keyIsDown(83) && s.directionY != blocksize * -1) {
+    s.directionX = 0;
+    s.directionY = blocksize;
+    //s
+  }
 
   background(220, 220, 220);
   //makes background a gray color
