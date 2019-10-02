76064833218d9dcef3386c8bbd0675c26d6ebc07
Kesler <45579249+OnlineEXP@users.noreply.github.com>
2019-10-01 20:32:50 -0700

    Update game.js

--- javascript/snake/game.js
+++ javascript/snake/game.js
@@ -1,45 +1,43 @@
-function snake(length, history) {
-  this.bodyLength = length;
-  this.bodyHistory = history;
-  this.directionX = 20;
-  this.directionY = 0;
-  //creates a basis for the snake including x,y, snake history, and length
-}
+var snakeLength = 3;
+var snakeHistory = [[640, 360], [660, 360], [680, 360]];
+var snakeDirX = 20;
+var snakeDirY = 0;
+//creates a basis for the snake including x,y, snake history, and length
 
 function setup() {
   createCanvas(1280, 720);
-  
+  background(220, 220, 220);
   frameRate(10);
   stroke(255, 255, 255);
-  s = new snake(3, [[640, 360], [660, 360], [680, 360]]);
+  s = new snake(snakeLength, snakeHistory);
 }
 
 function draw() {
-  if (keyIsDown(87) && s.directionY != 20) {
-    s.directionX = 0;
-    s.directionY = -20;
+  if (keyIsDown(87) && snakeDirY != 20) {
+    snakeDirX = 0;
+    snakeDirY = -20;
     //w
   }
-  if (keyIsDown(65) && s.directionX != 20) {
-    s.directionX = -20;
-    s.directionY = 0;
+  if (keyIsDown(65) && snakeDirX != 20) {
+    snakeDirX = -20;
+    snakeDirY = 0;
     //a
   }
-  if (keyIsDown(68) && s.directionX != -20) {
-    s.directionX = 20;
-    s.directionY = 0;
+  if (keyIsDown(68) && snakeDirX != -20) {
+    snakeDirX = 20;
+    snakeDirY = 0;
     //d
   }
-  if (keyIsDown(83) && s.directionY != -20) {
-    s.directionX = 0;
-    s.directionY = 20;
+  if (keyIsDown(83) && snakeDirY != -20) {
+    snakeDirX = 0;
+    snakeDirY = 20;
     //s
   }
 
   background(220, 220, 220);
   //makes background a gray color
   fill("black");
-  for (i = 0; i < s.bodyHistory.length; i++) {
-    rect(s.bodyHistory[i][0], s.bodyHistory[i][1], 20, 20);
+  for (i = 0; i < snakeHistory.length; i++) {
+    rect(snakeHistory[i][0], snakeHistory[i][1], 20, 20);
   }
 }
