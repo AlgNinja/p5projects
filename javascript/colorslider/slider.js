d1122443c15da6983e1e58301ee10418a716d7d8
Chris <51767118+AlgNinja@users.noreply.github.com>
2019-11-25 12:24:08 -0500

    Update slider.js

--- javascript/colorslider/slider.js
+++ javascript/colorslider/slider.js
@@ -8,7 +8,8 @@ function draw() {
   var rslider = createSlider(0,255, 100);
   rslider.position(20, 20);
   var gslider = createSlider(0,255, 100);
-  
-  var rslider = createSlider(0,255, 100);
+  gslider.postion(20, 50);
+  var bslider = createSlider(0,255, 100);
+  bslider.position(20, 80);
 }
 
