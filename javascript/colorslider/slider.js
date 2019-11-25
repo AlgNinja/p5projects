8d78670a1d058a98d3b694f48f89cfa07b8937af
Chris <51767118+AlgNinja@users.noreply.github.com>
2019-11-25 08:38:54 -0500

    Update slider.js

--- javascript/colorslider/slider.js
+++ javascript/colorslider/slider.js
@@ -4,3 +4,11 @@ createCanvas(windowWidth, windowHeight);
 function windowResized() {
 resizeCanvas(windowWidth, windowHeight);
 }
+function draw() {
+  var rslider = createSlider(0,255, 100);
+  rslider.position(20, 20);
+  var gslider = createSlider(0,255, 100);
+  
+  var rslider = createSlider(0,255, 100);
+}
+
