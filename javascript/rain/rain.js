8c8338efe46580dc6783dd60eb39a0e0d8eab908
Chris <51767118+AlgNinja@users.noreply.github.com>
2020-04-29 02:57:23 -0400

    Create rain.js

---
+++ javascript/rain/rain.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0);
+}
+
+function draw() {
+  let pos = {
+    x: random(0, windowWidth),
+    y: random(0, windowHeight)
+  }
+  let color = {
+    r: random(0, 255),
+    g: random(0, 50),
+    b: random(0, 255),
+    a: random(0, 69)
+  }
+  noStroke();
+  fill(color.r, color.g, color.b, color.a);
+  ellipse(pos.x, pos.y, 25, 25);
+
+}
